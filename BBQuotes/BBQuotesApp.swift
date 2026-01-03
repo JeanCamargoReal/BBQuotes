2a986643eda0eb55f95bf204c02892589c7bd697
JeanCamargoReal <jeancamargoreal@icloud.com>
2026-01-02 23:10:20 -0300

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Jean Camargo on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
